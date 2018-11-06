00acc9ba59bd778da47ceb3e62c19beea39450a9
chixin <chiwangabc@163.com>
2018-11-06 14:48:29 +0800

    update

---
+++ receive.py
@@ -0,0 +1,16 @@
+#-*- coding:utf-8 -*-
+import pika
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+# 建立一个rabbitmq的连接
+connection = pika.BlockingConnection(pika.ConnectionParameters('http://47.97.102.218:5672'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+channel.basic_consume(callback,queue='hello',no_ack=True)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
+
+
---
+++ send.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 -*-
+import pika
+
+# 建立一个rabbitmq的连接
+connection = pika.BlockingConnection(pika.ConnectionParameters('http://47.97.102.218:5672'))
+channel = connection.channel()
+
+# 创建一个hello的队列将消息存入这个队列
+channel.queue_declare(queue='hello')
+# 使用默认的交换机exchange代表交换机的名字,routing_key指定队列,body表示发送的消息
+channel.basic_publish(exchange='',routing_key='hello',body='Hello World!')
+connection.close()
+print(" [x] Sent 'Hello World!'")
